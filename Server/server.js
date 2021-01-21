2dc9adcbf0639ad41a25265a6399141ed0301c90
Wout Knuiman <WT.Knuiman@student.han.nl>
2021-01-21 16:17:18 +0100

    hopefully it works now!

--- Server/server.js
+++ Server/server.js
@@ -3,26 +3,31 @@ const { v4: uuidv4 } = require('uuid');
 const WebSockets = require("./websockets.js")
 const ws = require('ws');
 const { reduce, initState, state, teams } = require("./reducer")
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 const app = require('./staticServe');
 app.set("port", PORT);
 
 const httpServer = require('http').createServer(app);
 const wsServer = new ws.Server({
-  server: httpServer,
-  path: "/ws",
+	server: httpServer,
+	path: "/ws",
 })
 
 
-initState(uuidv4().substring(30),uuidv4().substring(30))
+initState(uuidv4().substring(30), uuidv4().substring(30))
 
 wsServer.on("connection", (websocket) => {
-	WebSockets.add(uuidv4(),websocket);
+	WebSockets.add(uuidv4(), websocket);
 
 	websocket.on("message", (message) => {
-		message = JSON.parse(message);
-		reduce(message.type, message.payload, websocket)
+		try {
+			message = JSON.parse(message);
+			reduce(message.type, message.payload, websocket)
+		}
+		catch (error) {
+			console.log(error);
+		}
 	});
 
 	websocket.on("close", () => {
